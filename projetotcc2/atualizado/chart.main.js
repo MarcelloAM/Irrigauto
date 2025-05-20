28ec19cfbfbfd8c2b0a6b89b43d22eb0be630138
Rodrigo <cabralrodrigo787@gmail.com>
2025-05-19 23:45:06 -0400

    Integracao dados firebase com os graficos rosca

--- projetotcc2/atualizado/chart.main.js
+++ projetotcc2/atualizado/chart.main.js
@@ -1,13 +1,20 @@
-const ctx = document.getElementById('meuGraficoDeRosca').getContext('2d');
-const meuGraficoDeRosca = new Chart(ctx, {
-    type: 'doughnut',
-    data: {
-        datasets: [{
-        data: [32, 68],
-            backgroundColor: [
-                'rgb(19, 51, 34)',
-                'rgb(83, 117, 58)'
-            ],
-        }]
-    },
-});
+setTimeout(() => {
+    const ctx = document.getElementById('meuGraficoDeRosca').getContext('2d');
+    var a = document.getElementById("saidaTemperatura").textContent;
+    console.log(a)
+    const b = parseInt(a);
+    const meuGraficoDeRosca = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            datasets: [{
+            data: [b, 60-b],
+                backgroundColor: [
+                    'rgb(19, 51, 34)',
+                    'rgb(83, 117, 58)'
+                ],
+            }]
+        },
+    });
+}, 1000);
+
+
--- projetotcc2/atualizado/chart.main2.js
+++ projetotcc2/atualizado/chart.main2.js
@@ -1,13 +1,19 @@
-const ctx2 = document.getElementById('meuSegundoGraficoDeRosca').getContext('2d'); // Use um novo nome de variável (ctx2)
-const meuSegundoGraficoDeRosca = new Chart(ctx2, { // Use um novo nome de variável para a instância do gráfico
-    type: 'doughnut',
-    data: {
-        datasets: [{
-            data: [60, 40],
-            backgroundColor: [
-                'rgb(19, 51, 34)',
-                'rgb(83, 117, 58)'
-            ]
-        }]
-    },
-});
+setTimeout(() => {
+    const ctx2 = document.getElementById('meuSegundoGraficoDeRosca').getContext('2d'); // Use um novo nome de variável (ctx2)
+    var a = document.getElementById("saidaUmidade").textContent;
+    console.log(a)
+    const b = parseInt(a);
+    const meuSegundoGraficoDeRosca = new Chart(ctx2, { // Use um novo nome de variável para a instância do gráfico
+        type: 'doughnut',
+        data: {
+            datasets: [{
+                data: [b, 100 - b],
+                backgroundColor: [
+                    'rgb(19, 51, 34)',
+                    'rgb(83, 117, 58)'
+                ]
+            }]
+        },
+    });
+}, 1000);
+
